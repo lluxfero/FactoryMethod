c5383ad7d3070f2481b92eeeb8af7836e1bdd304
Анастасия <onastja02@gmail.com>
2023-04-01 20:50:15 +0300

    Добавьте файлы проекта.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,60 @@
+﻿Transport transport = new MechanicalTransport("Velomesto");
+Delivery delivery1 = transport.Create();
+
+transport = new ElectricTransport("Volt Race");
+Delivery delivery2 = transport.Create();
+
+Console.ReadLine();
+
+
+abstract class Transport
+{
+    public string Speed { get; set; }
+
+    public Transport(string s)
+    {
+        Speed = s;
+    }
+    // фабричный метод
+    abstract public Delivery Create();
+}
+
+class MechanicalTransport : Transport
+{
+    public MechanicalTransport(string n) : base(n)
+    { }
+
+    public override Delivery Create()
+    {
+        return new BicycleDelivery();
+    }
+}
+
+class ElectricTransport : Transport
+{
+    public ElectricTransport(string n) : base(n)
+    { }
+
+    public override Delivery Create()
+    {
+        return new ElectricScooterDelivery();
+    }
+}
+
+abstract class Delivery
+{ }
+
+class BicycleDelivery : Delivery
+{
+    public BicycleDelivery()
+    {
+        Console.WriteLine("Заказ был доставлен на велосипеде");
+    }
+}
+class ElectricScooterDelivery : Delivery
+{
+    public ElectricScooterDelivery()
+    {
+        Console.WriteLine("Заказ был доставлен на электросамокате");
+    }
+}
